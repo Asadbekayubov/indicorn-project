dce98c4553ed254dd8bf346723b92281cb839363
Asadulloh <ayubovasadbek33@gmail.com>
2023-11-02 11:00:20 +0500

    second page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const close_btn = document.querySelector(".close-btn");
 const open_btn = document.querySelector(".open-btn");
+const inner_nav_btns = document.querySelectorAll(".inner-nav-link");
 
 const nav = document.querySelector(".inner-nav");
 
@@ -12,4 +13,10 @@ close_btn.addEventListener("click", () => {
   nav.style.transform = "translateX(-100%)";
   close_btn.style.display = "none";
   open_btn.style.display = "block";
+});
+
+inner_nav_btns.forEach((item) => {
+  item.addEventListener("click", () => {
+    nav.style.transform = "translateX(-100%)";
+  });
 });
